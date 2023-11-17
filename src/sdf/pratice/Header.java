4c65ca61eba5a96e4e1f4a9e0befab4dcf2b3098
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2023-11-18 00:11:07 +0800

    got the highest and lowest ratings to work!

--- src/sdf/pratice/Header.java
+++ src/sdf/pratice/Header.java
@@ -1,12 +1,14 @@
 package sdf.pratice;
 import sdf.pratice.Main;
 
+
+
 public class Header {
     private String category;
     private String app;
-    private Float rating;
+    private static Float rating;
 
-    public Header(String category, String app, float rating) {
+    public Header(String category, String app, Float rating) {
         this.category = category;
         this.app = app;
         this.rating = rating;
@@ -16,34 +18,22 @@ public class Header {
         return category;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public String getApp() {
+        return app;
     }
 
     public Float getRating() {
         return rating;
     }
 
-    public void setRating(Float rating) {
-        this.rating = rating;
-    }
-
-    public String getApp() {
-        return app;
-    }
-
-    public void setApp(String app) {
-        this.app = app;
-    }
-
-    public Float getAvg() {
-        if (Integer.parseInt(category) != 0) {
-            return rating / Integer.parseInt(category);
+    // Hypothetical getAvg method
+    public static Float getAvg() {
+        // Perform your average calculation here
+        // This is just a hypothetical implementation, adjust as needed
+        if (rating != null && !Float.isNaN(rating)) {
+            return rating / 2; // Adjust this calculation based on your requirements
         } else {
-            // Handle division by zero
-            return null;
+            return null; // or handle it according to your logic
         }
     }
-
-    
 }
--- src/sdf/pratice/Main.java
+++ src/sdf/pratice/Main.java
@@ -5,13 +5,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import sdf.pratice.Header;
+import java.util.Map.Entry;
 
-public class Main{
 
-    public static void main (String[]args) throws IOException{
+public class Main {
+
+    public static void main(String[] args) throws IOException {
         if (args.length <= 0) {
             System.out.println("Please insert CSV file");
             System.exit(1);
@@ -38,52 +40,162 @@ public class Main{
             for (String category : categoryNames) {
                 System.out.println(category);
             }
+
+            Map<String, String> columnMap = readColumnsToMap(args[0], 1, 2);
+
+            // Assuming you want to print or use the columnMap
+            for (Map.Entry<String, String> entry : columnMap.entrySet()) {
+                System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+            }
+
+            Map<String, Float> result = calculateAverageRating(args[0], 1, 2 );
+            for (Map.Entry<String, Float> enter : result.entrySet()) {
+                System.out.printf("Category: %s, Average Rating: %.2f%n", enter.getKey(), enter.getValue());
+            }
+
+             System.out.println("Highest Ratings:");
+             Map<String, Float> highestRatingApps = analyzeRatings(args[0],0, 1, 2 );
+                    for (Entry<String, Float> entry : highestRatingApps.entrySet()) {
+                        System.out.printf("Category: %s, Highest Rating App: %s, Rating: %.2f%n",
+                                entry.getKey(), entry.getValue(), highestRatingApps.get(entry.getKey()));
+                    }
+            
+                    // Print the lowest ratings
+                    Map<String, Float> lowestRatingsApps = analyzeRatings(args[0],0, 1, 2 );
+                    System.out.println("Lowest Ratings:");
+                    for (Entry<String, Float> entry : lowestRatingsApps.entrySet()) {
+                        System.out.printf("Category: %s, Lowest Rating App: %s, Rating: %.2f%n",
+                                entry.getKey(), entry.getValue(), lowestRatingsApps.get(entry.getKey()));
+                    }
+
+
+        }
+
+     }
+    
+
+    private static Map<String, String> readColumnsToMap(String filePath, int column1Index, int column2Index)
+            throws IOException {
+        Map<String, String> columnMap = new HashMap<>();
+
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+            // Skip the header if needed
+            bufferedReader.readLine();
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] columns = line.split(",");
+                if (columns.length > Math.max(column1Index, column2Index)) {
+                    String key = columns[column1Index].trim();
+                    String value = columns[column2Index].trim();
+                    if (!value.equalsIgnoreCase("NaN")) {
+                        columnMap.put(key, value);
+                }
+            }
+        }
+
+        return columnMap;
+    }
+ }
+
+ private static Map<String, Float> calculateAverageRating (String filePath, int column1Index, int column2Index) throws IOException {
+    Map<String, Float> averageRatingMap = new HashMap<>();
+    Map<String, Integer> categoryCountMap = new HashMap<>();
+
+    try (FileReader fileReader = new FileReader(filePath);
+         BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+        // Skip the header if needed
+        bufferedReader.readLine();
+
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] columns = line.split(",");
+            if (columns.length > Math.max(column1Index, column2Index)) {
+                String category = columns[column1Index].trim();
+                String ratingStr = columns[column2Index].trim();
+
+                // Check for NaN before adding to the computation
+                if (!ratingStr.equalsIgnoreCase("NaN")) {
+                    float rating = Float.parseFloat(ratingStr);
+
+                    // Update total rating for the category
+                    averageRatingMap.put(category, averageRatingMap.getOrDefault(category, 0f) + rating);
+
+                    // Update count for the category
+                    categoryCountMap.put(category, categoryCountMap.getOrDefault(category, 0) + 1);
+                }
+            }
         }
     }
 
+    // Calculate and store the average rating for each category
+    Map<String, Float> result = new HashMap<>();
+    for (Map.Entry<String, Integer> entry : categoryCountMap.entrySet()) {
+        String category = entry.getKey();
+        int count = entry.getValue();
+        float totalRating = averageRatingMap.getOrDefault(category, 0f);
 
-    public static void categorize(BufferedReader br) throws IOException {
-        br.readLine(); // Skip the header
+        if (count > 0) {
+            float averageRating = totalRating / count;
+            result.put(category, averageRating);
+        
+        }
 
-        Map<String, Float> categoryTotalRating = new HashMap<>();
-        Map<String, Integer> categoryCount = new HashMap<>();
+    }
 
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] columns = line.trim().split(",");
-            Header app = new Header(columns[0], columns[1], Float.parseFloat(columns[2]));
+    return result;
 
-            // Assuming Header.getAvg() calculates the average based on existing data.
-            Float avg = app.getAvg();
+    }
 
-            // Update categoryTotalRating and categoryCount for each category
-            if (avg != null) {
-                String category = app.getCategory();
+    private static Map<String, Float> analyzeRatings(String filePath, int appIndex, int categoryIndex, int ratingIndex) throws IOException {
+        Map<String, String> highestRatingApps = new HashMap<>();
+        Map<String, String> lowestRatingApps = new HashMap<>();
+        Map<String, Float> highestRatings = new HashMap<>();
+        Map<String, Float> lowestRatings = new HashMap<>();
 
-                // Update total rating for the category
-                categoryTotalRating.put(category, categoryTotalRating.getOrDefault(category, 0f) + avg);
+        try (FileReader fileReader = new FileReader(filePath);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
 
-                // Update count for the category
-                categoryCount.put(category, categoryCount.getOrDefault(category, 0) + 1);
+            // Skip the header if needed
+            bufferedReader.readLine();
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] columns = line.split(",");
+                if (columns.length > Math.max(appIndex, Math.max(categoryIndex, ratingIndex))) {
+                    String app = columns[appIndex].trim();
+                    String category = columns[categoryIndex].trim();
+                    String ratingStr = columns[ratingIndex].trim();
+
+                    if (!ratingStr.equalsIgnoreCase("NaN")) {
+                        float rating = Float.parseFloat(ratingStr);
+
+                        // Update highest rating for the category
+                        if (!highestRatingApps.containsKey(category) || rating > highestRatings.get(category)) {
+                            highestRatingApps.put(category, app);
+                            highestRatings.put(category, rating);
+                        }
+
+                        // Update lowest rating for the category
+                        if (!lowestRatingApps.containsKey(category) || rating < lowestRatings.get(category)) {
+                            lowestRatingApps.put(category, app);
+                            lowestRatings.put(category, rating);
+                        }
+                    }                    
+                }
+
+
             }
         }
-    
-
-        // Calculate and print the average rating for each category
-        for (Map.Entry<String, Float> entry : categoryTotalRating.entrySet()) {
-            String category = entry.getKey();
-            float totalRating = entry.getValue();
-            int count = categoryCount.get(category);
-
-            float averageRating = totalRating / count;
-
-            System.out.printf("Category: %s, Average Rating: %.2f%n", category, averageRating);
-        }
-    
+        return lowestRatings;
 
     }
 }
 
+
+
     
-
-
+   
