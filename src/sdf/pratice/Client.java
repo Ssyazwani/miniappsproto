6f7ee1b4d83ab128943cd138e965d90f77e2ece4
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2023-11-18 02:59:53 +0800

    starting on task02

--- src/sdf/pratice/Client.java
+++ src/sdf/pratice/Client.java
@@ -1,5 +1,30 @@
 package sdf.pratice;
 
+import java.net.Socket;
+
 public class Client {
+
+    public static void main(String[] args) {
+        int port = 3000;
+      String localhost = "";
+      switch (args.length) {
+         case 1:
+            localhost = args[0];
+            break;
+         case 2:
+            localhost = args[0];
+            port = Integer.parseInt(args[1]);
+            break;
+         default:
+            port = Integer.parseInt(args[0]);
+      }
+      System.out.println("Connected");
+
+       Socket socket = new Socket();
+       Program prog = new Program(socket);
+
+     
+
     
+}
 }
---
+++ src/sdf/pratice/Program.java
@@ -0,0 +1,10 @@
+package sdf.pratice;
+
+import java.net.Socket;
+
+public class Program {
+
+    public Program(Socket socket) {
+    }
+
+}
